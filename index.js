4fc5c7f6f6aef32f67b4b29856efb5bdd76d6e01
Zach Cullen <zacullen@gmail.com>
2020-09-13 22:06:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,18 @@ function reverseString(string) {
   if (string.length === 0) {
     return string
   }
-  
   return reverseString(string.substring(1, string.length)) + string[0]
+}
+
+function isPalindrome(string) {
+  // a palindrome is two equivalent sides, one of which reversed
+  // it matches from the outside in 
+  // check first and last equal? if not return false, if true pass sub string
+  // if string length less than 2 return true
+  if (string.length < 2) return true
+  if (string[0] === string[string.length - 1]) {
+    return isPalindrome(string.substring(1, stringl.length - 1))
+  } else {
+    return false
+  }
 }
