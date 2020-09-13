dac90dda6e4dae2c6c1788089fa23b8a2cfa61de
Zach Cullen <zacullen@gmail.com>
2020-09-13 21:43:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // Code your solution here!
-
+function printString(string) {
+  let substring
+  if (string.length > 1) {
+    substring = string.substring(1, string.length)
+    printString(substring)
+  } else {
+    return true
+  }
+}
